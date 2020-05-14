71562258d4a09a76742296582e8de0127a05ed5e
Alexander <devopsalexandr@gmail.com>
2020-05-14 13:44:44 +0400

    Routes added

--- JusTalk.Web/Controllers/IdentityController.cs
+++ JusTalk.Web/Controllers/IdentityController.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Threading.Tasks;
+using JusTalk.Web.Contracts.v1;
 using JusTalk.Web.Contracts.v1.Requests.Identity;
 using JusTalk.Web.Controllers.v1;
 using Microsoft.AspNetCore.Mvc;
@@ -8,6 +9,7 @@ namespace JusTalk.Web.Controllers
 {
     public class IdentityController : ApiController
     {
+        [HttpPost(ApiRoutes.Authentication.Login)]
         public async Task<IActionResult> Login([FromBody] PhoneNumberRequest phoneNumberRequest)
         {
             if (phoneNumberRequest == null) throw new ArgumentNullException(nameof(phoneNumberRequest));
@@ -15,6 +17,7 @@ namespace JusTalk.Web.Controllers
             return Ok();
         }
 
+        [HttpPost(ApiRoutes.Authentication.Confirm)]
         public async Task<IActionResult> ConfirmAuth([FromBody] ConfirmPhoneRequest confirmPhoneRequest)
         {
             if (confirmPhoneRequest == null) throw new ArgumentNullException(nameof(confirmPhoneRequest));
